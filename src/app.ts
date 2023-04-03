ed652a6a809343a371f0db73fd744493a4e3ca7a
gabriel <gabrielmelo98@hotmail.com>
2023-04-03 15:33:54 -0300

    ADD: NODE BASIC STRUCTURE

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from "express"
+import "express-async-errors"
+import "reflect-metadata"
+import handleError from './errors/handleError'
+
+
+const app = express()
+app.use(express.json())
+app.use(handleError)
+
+
+export default app
---
+++ src/errors/handleError.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express'
+import AppError from './appError'
+
+const handleError = async (error: Error, req: Request, resp: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return resp.status(error.statusCode).json({ message: error.message, })
+    }
+    return resp.status(500).json({
+        message: 'Internal server error',
+    })
+}
+
+export default handleError
